24149fb59f42eaa9060ec4bdd278c3e9f14c5a7d
samuelgarciacorrea <66627447+samuelgarciacorrea@users.noreply.github.com>
2024-02-29 12:22:50 -0500

    Tutorial 3a

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path 
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductoValido
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductoValido, CartView, CartRemoveAllView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name = 'home'),
     path("about/", AboutPageView.as_view(), name = 'about'),
+    path('contact', ContactPageView.as_view(), name = 'contact'),
     path('products/', ProductIndexView.as_view(), name = 'index'),
     path('products/valido', ProductoValido.as_view(), name = 'valido'),
     path('products/create', ProductCreateView.as_view(), name = 'form'),
     path('products/<str:id>', ProductShowView.as_view(), name = 'show'),
+    path('cart/', CartView.as_view(), name = 'cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name = 'cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name = 'cart_removeAll'),
 ]
